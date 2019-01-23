77f202847eb1a7a35d2645001b9727261dae4448
nicolemessier <nicolemessier@localprojects.com>
2019-01-23 14:46:08 -0500

    Added simpler client to send only unique IDs of generic trackers and their positions.

--- client/vivetracker_client-send-unqiue-id-and-position.py
+++ client/vivetracker_client-send-unqiue-id-and-position.py
@@ -18,6 +18,9 @@ interval = 1.0/args.fps
  
 print("OSC client is set to {0}:{1} at {2} fps".format(args.ip, args.port, args.fps))
 
+# Create unique ID's (beginning at 0, 1, ...) for each new generic controller
+serialNumberToUniqueID = {}
+
 if interval:
 	while(True):
 		start = time.time()
@@ -26,15 +29,29 @@ if interval:
 		for i in range(openvr.k_unMaxTrackedDeviceCount):
 			if poses[i].bPoseIsValid:
 				device_class = vr.getTrackedDeviceClass(i)
-				serial = vr.getStringTrackedDeviceProperty(i, openvr.Prop_SerialNumber_String).decode('utf-8')
-				m = poses[i].mDeviceToAbsoluteTracking
-				values = [serial, int(device_class),  
-							m[0][0], m[1][0], m[2][0], 
-							m[0][1], m[1][1], m[2][1],
-							m[0][2], m[1][2], m[2][2],  
-							m[0][3], m[1][3], m[2][3]  
-							]
-				client.send_message("/vive", values)
+
+				# Only allow generic controllers to pass through
+				if (device_class == 3):
+
+					# Get the serial number
+					serial = vr.getStringTrackedDeviceProperty(i, openvr.Prop_SerialNumber_String).decode('utf-8')
+					# Create a unique ID for this device if it doesn't already exist
+					if not serial in serialNumberToUniqueID:
+						thisID = len(serialNumberToUniqueID)
+						serialNumberToUniqueID[serial] = thisID
+
+					# Get the full transformation matrix
+					m = poses[i].mDeviceToAbsoluteTracking
+
+					# Get the position from this matrix
+					position = [m[0][3], m[1][3], m[2][3]]
+					
+					# Choose which values to send
+					values = [serialNumberToUniqueID[serial], position[0], position[1], position[2] ]
+
+					# Send this message with the header "vive"
+					client.send_message("/vive", values)
+
 		print("\r{0}".format(start), end="")
 		if sleep_time>0:
 			time.sleep(sleep_time)
